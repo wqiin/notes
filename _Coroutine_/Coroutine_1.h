bc420bc68b582bffd3dfa5489a9b11bf1671de03
wqiin <11366496+wqiin@user.noreply.gitee.com>
2025-01-02 20:03:16 +0800

    newly-added sequence generator by coroutine

--- _Coroutine_/Coroutine_1.h
+++ _Coroutine_/Coroutine_1.h
@@ -6,6 +6,8 @@
 #include <future>
 #include <iostream>
 
+#include <utility>  // std::exchange
+
 namespace TEST_1{
 
 class MyCoroutine{
@@ -61,6 +63,106 @@ private:
     handle_t handle;
 };
 
+
+class Generator{
+public:
+    struct promise_type{//the name is a MUST
+    public:
+        int value = 0;
+        bool bIsReady = false;
+
+        Generator get_return_object(){
+            return Generator(handle_t::from_promise(*this));
+        }
+
+        std::suspend_never initial_suspend(){
+            return {};
+        }
+
+        std::suspend_always final_suspend() noexcept{
+            return {};
+        }
+
+        //suspended when pass the value
+        //after await_transform being defined，co_await expr means to calling co_await promise.await_transform(expr)
+        std::suspend_always await_transform(int value) {
+            this->value = value;
+            return {};
+        }
+
+
+        std::suspend_always yield_value(int val){
+            std::cout << "yield calling\n";
+            this->value = val;
+            this->bIsReady = true;
+            return {};
+        }
+
+        //void return_void(){}//only one of the both can be defined
+        void return_value(int val){
+            std::cout << "return value calling\n";
+        }
+
+        void unhandled_exception(){//a MUST complememtion
+            std::exit(1);
+        }
+    };
+
+    using handle_t = std::coroutine_handle<promise_type>;
+    Generator(handle_t h):handle(h){};
+
+    //make Generator to control the coroutine object, in case of illegal pointer access
+    ~Generator(){
+        if(handle)
+            handle.destroy();
+    }
+
+    Generator(Generator && other) noexcept : handle(std::exchange(other.handle, {}))
+    {}
+    Generator & operator = (Generator && other){
+        this->handle = std::exchange(other.handle, {});
+    }
+
+    Generator(const Generator &) = delete;
+    Generator & operator = (const Generator &) = delete;
+
+    void resume(){
+        if(handle)
+            handle.resume();
+    }
+
+    bool done(){
+        if(handle)
+            return handle.done();
+
+        return false;
+    }
+
+
+    bool has_next(){
+        if(!handle || handle.done())
+            return false;
+
+        if(!handle.promise().bIsReady)
+            handle.resume();
+
+        if(handle.done())
+            return false;
+
+        return true;
+    }
+
+    int next(){
+        if(!has_next())
+            throw std::runtime_error("No Value");
+
+        handle.promise().bIsReady = false;
+        return handle.promise().value;
+    }
+
+private:
+    handle_t handle;
+};
 
 }
 #endif // COROUTINE_1_H
--- _Coroutine_/main.cpp
+++ _Coroutine_/main.cpp
@@ -21,7 +21,7 @@ TEST_1::MyCoroutine func1(){
 
 }
 
-int func(int a, int b){
+int returnInt(int a, int b){
     return a + b;
 }
 
@@ -41,21 +41,40 @@ float returnFloat(){
     return 0.0f;
 }
 
+TEST_1::Generator sequence(){
+    int i = 0;
+    while(i < 5){
+        co_yield i++;
+    }
+}
+
 int main(int argc, char *argv[])
 {
-    //a function without no explicit return type could lead to a compilation error,as the below
+    //a function without no explicit return type could lead to a compilation error,as the following
     // auto nullDefaultVal = getRetType([](){
     //     return ;
     // });
 
-    auto typeFaultVal = getRetType(func, 2, 3);
+    auto gen = sequence();
+    for(size_t ii = 0; ii < 15; ii++){
+        if(gen.has_next())
+            std::cout << gen.next() << std::endl;
+    }
+
+    /*
+    auto strDefault = getRetType([]()->std::string{
+        return {};
+    });
+     std::cout << "ret default string:" << strDefault << std::endl;
+
+
+    auto typeFaultVal = getRetType(returnInt, 2, 3);
     std::cout << "ret default val:" << typeFaultVal << std::endl;
 
     auto floatDefault = getRetType(returnFloat);
     std::cout << "float default val:" << floatDefault << std::endl;
 
     auto ptrDefaultVal = getRetType(returnPtr);
-    //std::cout << "pointer default val:" << static_cast<void*>(ptrDefaultVal) << std::endl;
     std::cout << "pointer default val:" << reinterpret_cast<void*>(ptrDefaultVal) << std::endl;
     return 0;
 
@@ -63,21 +82,23 @@ int main(int argc, char *argv[])
     //decltype(func) reutrn the type of the fucntioin, not the returned type pf the function. Arguments should be passed when calling the packaged_task object, though arguments has already been bound before
     //std::packaged_task<decltype(func)> pt1(std::bind(func, 2, 3));
 
-    std::packaged_task<int()> pt(std::bind(func, 2, 3));
+    std::packaged_task<int()> pt(std::bind(returnInt, 2, 3));
     auto future = pt.get_future();
     std::thread([task = std::move(pt)]() mutable{
         task();
     }).detach();
 
+    // auto asyncResult = std::async(std::launch::async, std::bind(returnInt, 11, 22));
+    // auto asyncFuture = asyncResult.get();
+
     std::cout << "value async:" << future.get() << std::endl;
     return 0;
+    */
 
+    // std::cout << "threads count:" << std::thread::hardware_concurrency() << std::endl;
+    // std::thread th1;
 
-    std::cout << "threads count:" << std::thread::hardware_concurrency() << std::endl;
-    std::thread th1;
-    //std::packaged_task<decltype(&func)()> pt(std::bind(func, 1, 2));
-    //pt.get_future();
-
+    /*
     auto cor1 = func1();
     std::cout << "main begin \n";
     cor1.resume();
@@ -93,6 +114,6 @@ int main(int argc, char *argv[])
 
     std::cout << "main ending\n";
     std::cout << "done:?" << cor1.done() << std::endl;;
-
+    */
     return 0;
 }
